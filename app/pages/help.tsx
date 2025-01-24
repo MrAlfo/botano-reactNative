2f89be4e3c70721de505b44ba65233b6e723a430
MrAlfo <alperengurbuz7@gmail.com>
2025-01-24 16:16:44 +0300

    Bottomsheet is added

--- app/pages/help.tsx
+++ app/pages/help.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
-import { Text, StyleSheet, View, ScrollView, TouchableOpacity, Pressable, SafeAreaView } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Pressable,
+  SafeAreaView,
+} from "react-native";
 import { Image } from "expo-image";
 import { ExerciseCard } from "@/components/ExerciseCard";
 import { ProductCard } from "@/components/ProductCard";
 import { RelatedProduct } from "@/components/RelatedProduct";
 import { PriceDisplay } from "@/components/PriceDisplay";
 import BottomSheetModal from "@/components/BottomSheetModal";
-import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withDelay, withTiming } from "react-native-reanimated";
-import { useRouter } from "expo-router";
+import Animated, {
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from "react-native-reanimated";
+import { Stack, useRouter } from "expo-router";
 
 interface BottomSheetProps {
   isOpen: Animated.SharedValue<boolean>;
@@ -48,7 +62,8 @@ function BottomSheet({
         onLayout={(e) => {
           height.value = e.nativeEvent.layout.height;
         }}
-        style={[sheetStyles.sheet, sheetStyle]}>
+        style={[sheetStyles.sheet, sheetStyle]}
+      >
         {children}
       </Animated.View>
     </>
@@ -68,14 +83,20 @@ const KartModal = () => {
   const closeSheet = () => setIsSheetVisible(false);
   return (
     <SafeAreaView>
+      <Stack.Screen options={{ headerShown: false }} />
       <ScrollView style={styles.screenContainer}>
-      <TouchableOpacity style={styles.backButton} onPress={() => router.push("/")}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => router.push("/")}
+        >
           <Text style={styles.backText}>X</Text>
         </TouchableOpacity>
 
         <View style={styles.mainContent}>
           <Image
-            source={{ uri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/e01f919a-13da-4e19-9516-3487e084c232?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0' }}
+            source={{
+              uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/e01f919a-13da-4e19-9516-3487e084c232?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0",
+            }}
             style={styles.mainImage}
           />
 
@@ -87,7 +108,9 @@ const KartModal = () => {
             />
 
             <Image
-              source={{ uri: 'https://cdn.builder.io/api/v1/image/assets/TEMP/e58faa22cce2424a22821644f3a7ae4710ea7bff70c4962ca480599f473b1580?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0' }}
+              source={{
+                uri: "https://cdn.builder.io/api/v1/image/assets/TEMP/e58faa22cce2424a22821644f3a7ae4710ea7bff70c4962ca480599f473b1580?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0",
+              }}
               style={styles.dividerImage}
             />
 
@@ -105,7 +128,10 @@ const KartModal = () => {
               title="Advanced Juice Reboot by JRINK by Purée Regular price"
               price="$150"
               subtitle="Sustainably Wild Harvested Chaga Mushroom"
-              images={['https://cdn.builder.io/api/v1/image/assets/TEMP/00a158dfd32bdb68f526b60eb767973e4037a8c27231b1bf40233e59293b1f9f?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0', 'https://cdn.builder.io/api/v1/image/assets/TEMP/a0f3dabfa93ecdb1ad1ecfa5a3707b98e904a4f8f83e61b4e41f4f7836782ee2?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0']}
+              images={[
+                "https://cdn.builder.io/api/v1/image/assets/TEMP/00a158dfd32bdb68f526b60eb767973e4037a8c27231b1bf40233e59293b1f9f?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0",
+                "https://cdn.builder.io/api/v1/image/assets/TEMP/a0f3dabfa93ecdb1ad1ecfa5a3707b98e904a4f8f83e61b4e41f4f7836782ee2?placeholderIfAbsent=true&apiKey=7efa2bc6ca0748d89d049010d54aaee0",
+              ]}
             />
             <TouchableOpacity style={styles.actionButton} onPress={toggleSheet}>
               <Text style={styles.actionButtonText}>Ok, I'm in!</Text>
@@ -114,21 +140,75 @@ const KartModal = () => {
             {isSheetVisible && <BottomSheetModal closeSheet={closeSheet} />}
           </View>
         </View>
-
       </ScrollView>
       <BottomSheet isOpen={isOpen} toggleSheet={toggleSheet}>
-        <Animated.Text>
-          Discover the indispensable convenience of a bottom sheet in mobile
-          app. Seamlessly integrated, it provides quick access to supplementary
-          features and refined details.
-        </Animated.Text>
-        <View style={styles.buttonContainer}>
-          <Pressable style={[styles.bottomSheetButton]}>
-            <Text style={[styles.bottomSheetButtonText]}>
-              Read mores
+        <ScrollView showsVerticalScrollIndicator={false} style={styles.sheetContent}>
+          <Text style={styles.cardCompletedText}>CARD COMPLETED!</Text>
+          <Text style={styles.greatText}>Great!</Text>
+          <Text style={styles.subtitle}>
+            Plant Care Day{" "}
+            <Text style={styles.subtitleBold}>
+              Kartını başarıyla tamamladınız.
             </Text>
-          </Pressable>
-        </View>
+          </Text>
+          <View style={styles.pointsContainer}>
+            <View style={styles.circle}>
+              <Text style={styles.pointText}>5</Text>
+              <Image
+                source={require("../../assets/images/leaf.png")}
+                style={styles.leafIcon}
+              />
+            </View>
+          </View>
+          <Text style={styles.extraPointsText}>
+            Daha fazla puan kazanmak için{"\n"}
+            <Text style={styles.extraPointsBold}>video yükleyebilirsiniz.</Text>
+          </Text>
+          <View style={styles.iconsContainer}>
+            <View style={styles.iconBox}>
+              <Image
+                // source={require("./assets/camera.png")}
+                style={styles.iconImage}
+              />
+              <Text style={styles.iconText}>+1</Text>
+            </View>
+            <View style={styles.iconBox}>
+              <Image
+                // source={require("./assets/mic.png")}
+                style={styles.iconImage}
+              />
+              <Text style={styles.iconText}>+1</Text>
+            </View>
+            <View style={[styles.iconBox, styles.disabled]}>
+              <Image
+                // source={require("./assets/video.png")}
+                style={styles.iconImage}
+              />
+              <Text style={styles.iconText}>-</Text>
+            </View>
+          </View>
+          <View style={styles.nextCardContainer}>
+            <Text style={styles.nextCardTitle}>RELATED CARDS</Text>
+            <Text style={styles.cardTitle}>Plant Care Day</Text>
+            <View style={styles.nextCardContent}>
+              <TouchableOpacity>
+                <Text style={styles.arrowText}>{"<"}</Text>
+              </TouchableOpacity>
+              <View style={styles.card}>
+                <Image
+                  source={require("../../assets/images/woman.png")}
+                  style={styles.cardImage}
+                />
+              </View>
+              <TouchableOpacity>
+                <Text style={styles.arrowText}>{">"}</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.newCardButton}>
+              <Text style={styles.newCardButtonText}>CONTINUE NEW CARD</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </BottomSheet>
     </SafeAreaView>
   );
@@ -136,50 +216,54 @@ const KartModal = () => {
 
 const styles = StyleSheet.create({
   screenContainer: {
-    backgroundColor: '#38635A',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    width: '100%',
+    backgroundColor: "#38635A",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "100%",
     paddingTop: 21,
     paddingBottom: 21,
-    fontFamily: 'Poppins_Regular',
+    fontFamily: "Poppins_Regular",
+  },
+  sheetContent: {
+    flex: 1,
   },
   backButton: {
     borderRadius: 90,
-    borderColor: '#FFFFFF',
+    borderColor: "#FFFFFF",
     borderWidth: 2,
     maxHeight: 40,
     padding: 8,
-    alignItems: 'center',
+    alignItems: "center",
     width: 40,
-    right: 10,
-    top: 10,
-    position: 'absolute',
-    zIndex: 3
+    right: 20,
+    top: 20,
+    position: "absolute",
+    zIndex: 3,
+    opacity: 0.45,
   },
   backIcon: {
     width: 14,
     aspectRatio: 1,
-    color: '#fff',
+    color: "#fff",
   },
   backText: {
-    color: '#fff',
+    color: "#fff",
   },
   mainContent: {
     marginTop: 80,
-    width: '100%',
+    width: "100%",
     paddingLeft: 20,
     marginBottom: 40,
   },
   mainImage: {
-    width: '100%',
+    width: "100%",
     aspectRatio: 0.75,
   },
   contentContainer: {
     marginTop: 20,
     minHeight: 852,
-    width: '100%',
-    paddingEnd: 20
+    width: "100%",
+    paddingEnd: 20,
   },
   dividerImage: {
     marginTop: 40,
@@ -191,19 +275,19 @@ const styles = StyleSheet.create({
     borderRadius: 40,
     marginTop: 29,
     padding: 16,
-    borderColor: '#ffffff',
+    borderColor: "#ffffff",
     borderWidth: 1,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff",
   },
   actionButtonText: {
     fontSize: 20,
-    color: '#38635A',
-    fontFamily: 'Poppins_Regular',
-    fontWeight: '600',
-    textAlign: 'center',
+    color: "#38635A",
+    fontFamily: "Poppins_Regular",
+    fontWeight: "600",
+    textAlign: "center",
   },
   text: {
-    fontFamily: 'Poppins_Regular',
+    fontFamily: "Poppins_Regular",
   },
   flex: {
     flex: 1,
@@ -245,22 +329,198 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "600",
   },
+  containerModal: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    borderTopLeftRadius: 47,
+    borderTopRightRadius: 47,
+    paddingHorizontal: 20,
+    paddingTop: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  modalHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  backButtonModal: {
+    width: 35,
+    height: 35,
+  },
+  backIconModal: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  cardCompletedText: {
+    fontSize: 16,
+    fontWeight: "400",
+    textTransform: "uppercase",
+    color: "rgba(0, 0, 0, 0.4)",
+    textAlign: "center",
+  },
+  greatText: {
+    fontSize: 24,
+    fontWeight: "800",
+    fontFamily: "Poppins_Bold",
+    textAlign: "center",
+    color: "#323534",
+    marginVertical: 10,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontFamily: "Poppins_Regular",
+    fontSize: 14,
+    lineHeight: 20,
+    color: "#323534",
+    marginBottom: 20,
+    fontWeight: "700",
+  },
+  subtitleBold: {
+    fontWeight: "400",
+  },
+  pointsContainer: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  circle: {
+    width: 109,
+    height: 109,
+    borderRadius: 109 / 2,
+    backgroundColor: "#B6D9ED",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+  pointText: {
+    fontSize: 50,
+    fontWeight: "700",
+    color: "#000",
+  },
+  leafIcon: {
+    width: 42,
+    height: 42,
+    position: "absolute",
+    right: 0,
+    top: 0,
+  },
+  extraPointsText: {
+    textAlign: "center",
+    fontSize: 14,
+    lineHeight: 20,
+    color: "#323534",
+    marginBottom: 20,
+  },
+  extraPointsBold: {
+    fontWeight: "600",
+  },
+  iconsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  iconBox: {
+    width: 80,
+    height: 50,
+    borderRadius: 27.5,
+    borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.5)",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  iconImage: {
+    width: 24,
+    height: 24,
+    marginRight: 5,
+  },
+  iconText: {
+    fontSize: 18,
+    fontWeight: "700",
+    color: "#323534",
+  },
+  disabled: {
+    opacity: 0.3,
+    borderColor: "rgba(0, 0, 0, 0.7)",
+  },
+  nextCardContainer: {
+    marginTop: 30,
+    alignItems: "center",
+        backgroundColor: 'rgba(231, 242, 243, 0.7)',
+        borderRadius: 20
+  },
+  nextCardTitle: {
+    fontSize: 14,
+    fontWeight: "600",
+    color: "#323534",
+    marginBottom: 10,
+    marginTop: 20,
+    fontFamily: 'Poppins_Regular'
+  },
+  nextCardContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  card: {
+    width: 200,
+    height: 250,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 20,
+  },
+  cardImage: {
+    width: 160,
+    height: 180,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: "#323534",
+    textAlign: "center",
+    fontFamily: 'Poppins_Regular'
+  },
+  arrowText: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: "#323534",
+  },
+  newCardButton: {
+    backgroundColor: "#000",
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  newCardButtonText: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "#FFF",
+  },  
 });
 
 // BottomSheet için Stil
 const sheetStyles = StyleSheet.create({
   sheet: {
     padding: 16,
-    height: 150,
+    height: 600,
     width: "100%",
     position: "absolute",
     bottom: 0,
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
+    borderTopRightRadius: 47,
+    borderTopLeftRadius: 47,
     zIndex: 2,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: '#fff'
+    backgroundColor: "#fff",
   },
   backdrop: {
     ...StyleSheet.absoluteFillObject,
@@ -269,4 +529,3 @@ const sheetStyles = StyleSheet.create({
 });
 
 export default KartModal;
-
